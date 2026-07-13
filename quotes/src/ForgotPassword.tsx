c6319d8642da7177316ce5c43793a32de987a29f
shrungpatel <shrungpatel@yahoo.in>
2026-07-13 18:51:03 -0500

    design changes to the forgot password page

--- quotes/src/ForgotPassword.tsx
+++ quotes/src/ForgotPassword.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import "./App.css";
 import { Button, Box } from "@mui/material/";
-import { Container, Link, TextField } from "@mui/material";
+import { Container, TextField } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 function ForgotPassword() {
@@ -16,17 +16,17 @@ function ForgotPassword() {
     <div className="App">
       <header className="App-header">
         <Box className="App-box">
+          <Button sx={{ position: "absolute", top: 25, left: 25 }} onClick={loginClick}>
+              &larr; {/* left arrow */}
+          </Button>
           <Container maxWidth="sm">
             <h1>Forgot Password</h1>
             <div>
               <TextField id="email" label="Enter your email" variant="filled" />
             </div>
           </Container>
-          <Link href="ResendEmail">{"Resend Email"}</Link>
           <br></br>
-          <Button onClick={loginClick} variant="contained">
-            Sign in!
-          </Button>
+          <Button variant="contained">Send Email</Button>
         </Box>
       </header>
     </div>
